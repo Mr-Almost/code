1ccd046be9fc1c88fef2c519009be9338e5b4d32
Mr-Almost <499158415@qq.com>
2018-03-15 17:36:40 +0800

    下载

--- canvas/main.js
+++ canvas/main.js
@@ -1,5 +1,6 @@
 var yyy=document.getElementById('xxx');
 var context =yyy.getContext('2d');
+var lineWidth=5;
 autoSetCanvasSize(yyy);
 listenToUser (yyy);
 
@@ -54,7 +55,30 @@ blue.classList.remove('active');
  green.classList.remove('active');
  blue.classList.add('active');  
   }
+
+  thin.onclick =function(){
+    lineWidth=5;
+
+  }
   
+  thick.onclick =function(){
+    lineWidth=10;
+  };
+
+  clear.onclick =function() {
+    context.clearRect(0,0, yyy.width,yyy.height);
+  }
+  download.onclick=function(){
+    var url =  yyy.toDataURL("image/png")
+   
+    var a= document.createElement('a')
+    document.body.appendChild(a)
+    a.href=url
+    a.download= '我的作品';
+    a.target='_blank'
+    a.click()
+   
+  }
 
 /*
 context.strokeStyle='yellow';
@@ -195,7 +219,7 @@ function drawCircle(x,y,radius){
 function drawline(x1,y1,x2,y2){
       context.beginPath();
       context.moveTo(x1,y1);
-      context.lineWidth =5;
+      context.lineWidth =lineWidth;
       context.lineTo(x2,y2);
       context.stroke();
       context.closePath();
